8ffea4ddc62ebffbf1171c11ff8618bc8bf51a2e
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-06-27 19:59:51 -0600

    Fixed movement

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-//File communication, will request files from server (images, etc.)
+//File comm. code, requests files from server (images, etc.)
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
-});
-
+})
 app.use('/client', express.static(__dirname + '/client'));
-server.listen(2000); //listening on port 2000
+server.listen(2000); //listens on port 2000
 console.log('Server started');
 
 var SOCKET_LIST = {}; //list of sockets being read
@@ -19,43 +18,43 @@ var Player = function(id) {
 		x:250,
 		y:250,
 		id:id,
-		number: "" + Math.floor(10 * Math.random()), //distinguishes different players on screen
-		pressingRight: false,
-		pressingLeft: false,
-		pressingUp: false,
-		pressingDown: false,
-		maxSpeed: 10
+		number:"" + Math.floor(10 * Math.random()), //used to distinguish diff players
+		pressingRight:false,
+		pressingLeft:false,
+		pressingUp:false,
+		pressingDown:false,
+		maxSpd:10
 	}
-	self.updatePosition = function() { //number will react to key the player presses
+	self.updatePosition = function() { //reacts based on user key press
 		if(self.pressingRight)
-			self.x += self.maxSpeed;
+			self.x += self.maxSpd;
 		if(self.pressingLeft)
-			self.x -= seld.maxSpeed;
+			self.x -= self.maxSpd;
 		if(self.pressingUp)
-			self.y -= self.maxSpeed;
+			self.y -= self.maxSpd;
 		if(self.pressingDown)
-			self.y += self.maxSpeed;
+			self.y += self.maxspd;
 	}
 	return self;
 }
 
-//socket.io configuration
-var io = require('socket.io')(server, {});
+//socket io config
+var io = require('socket.io')(server,{});
 io.sockets.on('connection', function(socket) {
 	socket.id = Math.random(); //unique id for each socket
 	SOCKET_LIST[socket.id] = socket; //add socket id to socket list
-
+	
 	var player = Player(socket.id);
 	PLAYER_LIST[socket.id] = player;
-
-	//when player disconnects, they will be be visually removed from the server
-	socket.on('disconnect', function() {
+	
+	//when player disconnects, they will be visually removed from server
+	socket.on('disconnect', function(){
 		delete SOCKET_LIST[socket.id];
 		delete PLAYER_LIST[socket.id];
 	})
-
-	//allows for keypress data to be received on the server side
-	socket.on('keyPress', function(data) {
+	
+	//allows for keypress data to be receieved on server end
+	socket.on('keyPress',function(data){
 		if(data.inputId === 'left')
 			player.pressingLeft = data.state;
 		else if(data.inputId === 'right')
@@ -67,12 +66,12 @@ io.sockets.on('connection', function(socket) {
 	})
 })
 
-setInterval(function() { //loops through each socket and sends a package containing new x&y positions 
-	var pack = []; //contains information for every player that joined
-	for(var i in PLAYER_LIST) {
+setInterval(function(){ //loops through each socket and sends package containing new x & y pos
+	var pack = []; //contains info for every player that joined
+	for(var i in PLAYER_LIST){
 		var player = PLAYER_LIST[i];
-		player.updatePosition(); //function that reacts to key press
-		pack.push({ //push the position of each player to package on the client
+		player.updatePosition(); //function that reacts to user key press
+		pack.push({ //push pos of each player to pack on client
 			x:player.x,
 			y:player.y,
 			number:player.number
@@ -80,6 +79,6 @@ setInterval(function() { //loops through each socket and sends a package contain
 	}
 	for(var i in SOCKET_LIST) {
 		var socket = SOCKET_LIST[i];
-		socket.emit('newPositions', pack);
+		socket.emit('newPositions',pack);
 	}
-}, 1000/25); //app will run at 25 frames per second
+},1000/25); //will run at 25 frames per second
