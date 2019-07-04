b25ff171a9e1692527cbf461eb4d5c14a5afb106
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-04 16:50:26 -0600

    Trying to fix crash

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
  
-serv.listen(process.env.PORT || 2000, function() {
+app.listen(process.env.PORT || 2000, function() {
 	console.log("Server listening on port %d", this.address().port, app.settings.env);
 });
  
