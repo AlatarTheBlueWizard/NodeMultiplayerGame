4da87be062bbe9a5697f5413f2e2ddf65a784e87
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 14:22:06 -0600

    Added bullet object and functionality

--- entity.js
+++ entity.js
@@ -243,3 +243,88 @@ Player.update = function(){
 	}
 	return pack;
 }
+
+//bullet object and config
+Bullet = function(param){
+	var self = Entity(param);
+	self.id = Math.random();
+	self.angle = param.angle;
+	self.spdX = Math.cos(param.angle/180*Math.PI) * 10;
+	self.spdY = Math.sin(param.angle/180*Math.PI) * 10;
+	self.parent = param.parent;
+
+	self.timer = 0;
+	self.toRemove = false;
+	var super_update = self.update;
+	self.update = function(){
+		if(self.timer++ > 100)
+			self.toRemove = true;
+		super_update();
+
+		for(var i in Player.list){
+			var p = Player.list[i];
+			if(self.map === p.map && self.getDistance(p) < 32 && self.parent !== p.id){
+
+				p.hp -= 1;
+
+				if(p.hp <= 0){
+					var shooter = Player.list[self.parent];
+					if(shooter)
+						shooter.score += 1;
+					p.hp = p.hpMax;
+					p.h = Math.random() * 500;
+					p.y = Math.random() * 500;
+				}
+				self.toRemove = true;
+			}
+		}
+	}
+	//get pack for bullet data
+	self.getInitPack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		};
+	}
+	self.getUpdatePack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+		};
+	}
+
+	//set id to bullet
+	Bullet.list[self.id] = self;
+	initPack.bullet.push(self.getInitPack());
+	return self;
+}
+
+//list of bullets
+Bullet.list = {};
+
+//bullet update config
+Bullet.update = function(){
+	var pack = [];
+	for(var i in Bullet.list){
+		var bullet = Bullet.list[i];
+		bullet.update();
+		if(bullet.toRemove){
+			delete Bullet.list[i];
+			removePack.bullet.push(bullet.id);
+		} else{
+			pack.push(bullet.getUpdatePack());
+		}
+	}
+	return pack;
+}
+
+//get bullet data
+Bullet.getAllInitPack = function(){
+	var bullets = [];
+	for(var i in Bullet.list)
+		bullets.push(Bullet.list[i].getInitPack());
+	return bullets;
+}
