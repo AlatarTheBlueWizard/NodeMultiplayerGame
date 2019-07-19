70cea77803c158cb5b0afb618c086a0d0d2a867c
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 13:51:41 -0600

    Added entity data retrieval

--- entity.js
+++ entity.js
@@ -57,3 +57,89 @@ Entity.getFrameUpdateData = function(){
 	removePack.bullet = [];
 	return pack;
 }
+
+//player data and object
+Player = function(param){
+	var self = Entity(param);
+	self.number = "" + Math.floor(10 * Math.random());
+	self.username = param.username;
+	self.pressingRight = false;
+	self.pressingLeft = false;
+	self.pressingUp = false;
+	self.pressingDown = false;
+	self.pressingAttack = false;
+	self.mouseAngle = 0;
+	self.maxSpd = 10;
+	self.hp = 10;
+	self.hpMax = 10;
+	self.score = 0;
+	self.inventory = new Inventory(param.progress.items,param.socket,true);
+
+	//super update
+	var super_update = self.update;
+	self.update = function(){
+		super_update();
+		if(self.pressingAttack){
+			self.shootBullet(self.mouseAngle);
+		}
+	}
+	//bullet config
+	self.shootBullet = function(angle){
+		if(Math.random() < 0.1)
+			self.inventory.addItem("potion",1);
+		Bullet({
+			parent:self.id,
+			angle:angle,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		});
+	}
+	//speed update config
+	self.updateSpd = function(){
+		if(self.pressingRight)
+			self.spdX = self.maxSpd;
+		else if(self.pressingLeft)
+			self.spdX = -self.maxSpd;
+		else
+			self.spdX = 0;
+
+		if(self.pressingUp)
+			self.spdY = -self.maxSpd;
+		else if(self.pressingDown)
+			self.spdY = self.maxSpd;
+		else
+			self.spdY = 0;
+	}
+	//retrieve entity data
+	self.getInitPack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			number:self.number,
+			hp:self.hp,
+			hpMax:self.hpMax,
+			score:self.score,
+			map:self.map,
+		};
+	}
+	//retrieve updated data
+	self.getUpdatePack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			hp:self.hp,
+			score:self.score,
+			map:self.map,
+		};
+	}
+
+	//set id to specific player in list
+	Player.list[self.id] = self;
+	//push new player to entity
+	initPack.player.push(self.getInitPack());
+	return self;
+}
+
