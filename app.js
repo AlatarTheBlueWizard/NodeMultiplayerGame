8f6b4aacc4da049ccfad560c0cb6589e4532d81f
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 16:18:42 -0600

    Trying to fix db

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //require databse, entity, and inventory files
-require('./Database.js');
-require('./Entity.js');
-require('./client/Inventory.js');
+require('./Database');
+require('./Entity');
+require('./client/Inventory');
 
 // modules needed for express and http
 var express = require('express');
