4ecb9f97fee3da978c689e1fcf2fb11238888c7c
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 14:44:29 -0600

    Going to add tables

--- Database.js
+++ Database.js
@@ -2,3 +2,6 @@ var USE_DB = true;
 var mongojs = USE_DB ? require("mongojs") : null;
 var db = USE_DB ? mongojs('', ['account','progress']) : null;
 
+//account table: {username:string, password:string}
+//progress table: {username:string, items:[{id:string,amount:number}]}
+
