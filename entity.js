9637c533562ac82cbcd802c37e9ccf80c4f4add6
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 13:39:23 -0600

    Added entity object and packs

---
+++ entity.js
@@ -0,0 +1,59 @@
+var initPack = {player:[],bullet:[]};
+var removePack = {player:[],bullet:[]};
+
+//Entity object
+Entity = function(param) {
+	var self = {
+		x:250,
+		y:250,
+		spdX:0,
+		spdY:0,
+		id:"",
+		map:'forest',
+	}
+	if(param) {
+		if(param.x)
+			self.x = param.x;
+		if(param.y)
+			self.y = param.y;
+		if(param.map)
+			self.map = param.map;
+		if(param.id)
+			self.id = param.id;
+	}
+
+	self.update = function(){
+		self.updatePosition();
+	}
+	self.updatePosition = function(){
+		self.x += self.spdX;
+		slef.y += self.spdY;
+	}
+	self.getDistance = function(pt){
+		return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+	}
+	return self;
+}
+
+//Entity frame handler
+Entity.getFrameUpdateData = function(){
+	var pack = {
+		initPack:{
+			player:initPack.player,
+			bullet:initPack.bullet,
+		},
+		removePack:{
+			player:removePack.player,
+			bullet:removePack.bullet,
+		},
+		updatePack:{
+			player:Player.update(),
+			bullet:Bullet.update(),
+		}
+	};
+	initPack.player = [];
+	initPack.bullet = [];
+	removePack.player = [];
+	removePack.bullet = [];
+	return pack;
+}
