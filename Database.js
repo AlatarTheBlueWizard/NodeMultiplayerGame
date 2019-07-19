590427eb40ddc51a05573644de3a6bdc81cdb7ff
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 16:08:24 -0600

    trying to fix connection

--- Database.js
+++ Database.js
@@ -4,9 +4,11 @@ var db = USE_DB ? mongojs('mongodb+srv://user:pass@mygame-4y1xa.mongodb.net/test
 */
 //account collection: {username:string, password:string}
 //progress collection: {username:string, items:[{id:string,amount:number}]}
-var mongodb = require('mongodb');
+var USE_DB = true;
+var mongodb = USE_DB ? require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb+srv://user:pass@mygame-4y1xa.mongodb.net/test?retryWrites=true&w=majority';
+var db = USE_DB ? url;
 
 MongoClient.connect(url,function(err,db){
 	if(err)
