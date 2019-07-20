7201e68ec46d13b28fd3d3d2cecade1765fa195e
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 19:42:39 -0600

    Edited requirments for a function

--- Database.js
+++ Database.js
@@ -6,6 +6,7 @@ var db = USE_DB ? mongojs('mongodb+srv://user:pass@mygame-4y1xa.mongodb.net/test
 //progress collection: {username:string, items:[{id:string,amount:number}]}
 var USE_DB = true;
 var mongodb = USE_DB ? require('mongodb') : null;
+var mongojs = USE_DB ? require("mongojs") : null;
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb+srv://user:pass@mygame-4y1xa.mongodb.net/test?retryWrites=true&w=majority';
 var db = USE_DB ? url : null;
