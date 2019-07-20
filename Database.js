02168312b2f4cabf7b742865790869c71d2e90d6
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-07-19 20:22:12 -0600

    Edited UI and db file

--- Database.js
+++ Database.js
@@ -4,12 +4,6 @@ var db = USE_DB ? mongojs('localhost:27017/myGame', ['account','progress']) : nu
 
 //account collection: {username:string, password:string}
 //progress collection: {username:string, items:[{id:string,amount:number}]}
-/*var USE_DB = true;
-var mongodb = USE_DB ? require('mongodb') : null;
-var mongojs = USE_DB ? require("mongojs") : null;
-var MongoClient = mongodb.MongoClient;
-var url = 'mongodb+srv://user:pass@mygame-4y1xa.mongodb.net/test?retryWrites=true&w=majority';
-var db = USE_DB ? mongodb(url, ['account','progress']) : null;*/
 
 Database = {};
 Database.isValidPassword = function(data,cb){
@@ -58,56 +52,3 @@ Database.isValidPassword = function(data,cb){
 				return cb();
 			db.progress.update({username:data.username},data,{upsert:true},cb);
 		}
-/*MongoClient.connect(url,function(err,db){
-	if(err)
-		console.log('Unable to connect to the mongoDB server. Error:', err);
-	else {
-		//Database = {};
-		Database.isValidPassword = function(data,cb){
-			if(!USE_DB)
-				return cb(true);
-			db.account.findOne({username:data.username,password:data.password},function(err,res){
-				if(res)
-					cb(true);
-				else
-					cb(false);
-			});
-		}
-
-		Database.isUsernameTaken = function(data,cb){
-			if(!USE_DB)
-				return cb(false);
-			db.account.findOne({username:data.username},function(err,res){
-				if(res)
-					cb(true);
-				else
-					cb(false);
-			});
-		}
-
-		Database.addUser = function(data,cb){
-			if(!USE_DB)
-				return cb();
-			db.account.insert({username:data.username,password:data.password},function(err){
-				Database.savePlayerProgress({username:data.username,items:[]},function(){
-            		cb();
-        		})
-			});
-		}
-
-		Database.getPlayerProgress = function(username,cb){
-			if(!USE_DB)
-				return cb({items:[]});
-			db.progress.findOne({username:username},function(err,res){
-				cb({items:res.items});
-			});
-		}
-
-		Database.savePlayerProgress = function(data,cb){
-			cb = cb || function(){}
-			if(!USE_DB)
-				return cb();
-			db.progress.update({username:data.username},data,{upsert:true},cb);
-		}
-	}
-});*/
